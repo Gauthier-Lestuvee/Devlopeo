aa4cdc8582a698475a41e6ce40c06b12be8378e9
Gauthier Lestuvée <80978986+Gauthier-Lestuvee@users.noreply.github.com>
2023-10-09 17:32:14 +0200

    Add files via upload

---
+++ js/typed.js
@@ -0,0 +1,33 @@
+var options = {
+    strings: ["Bienvenue", "chez Devlopeo."],
+    typeSpeed: 50, // Vitesse de frappe (en millisecondes)
+    backSpeed: 35, // Vitesse de suppression (en millisecondes)
+    startDelay: 500, // Délai avant de commencer à écrire
+    backDelay: 2500, // Délai après avoir effacé un texte
+    loop: true, // Répéter en boucle
+    showCursor: false,
+  };
+  
+  var typed = new Typed("#typed-output-welcome", options);
+
+var options = {
+    strings: ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero aspernatur repellat inventore deleniti reprehenderit magni earum facilis dolorum error cumque exercitationem quis non maiores iste atque, animi, ipsam dicta? Omnis dolorum asperiores iusto assumenda cumque officia a, molestiae sed. Voluptatibus, ipsam. Aut impedit cum voluptate vel nesciunt et maxime minima mollitia inventore amet fugit, laborum quisquam ratione architecto nihil enim soluta autem fugiat nobis laudantium totam maiores laboriosam! Aperiam quasi, ad praesentium autem nulla eligendi nobis labore magnam, excepturi eius at dignissimos possimus necessitatibus ut voluptatem neque quia in eos, molestiae distinctio quisquam! Explicabo laboriosam cumque eaque at vero ab."],
+    typeSpeed: 0.1, // Vitesse de frappe (en millisecondes)
+    startDelay: 550, // Délai avant de commencer à écrire
+    loop: false, // Répéter en boucle
+    showCursor: false,
+  };
+
+  var typed = new Typed("#typed-output-description", options);
+
+var options = {
+    strings: ["HTML", "CSS", "JS"],
+    typeSpeed: 100, // Vitesse de frappe (en millisecondes)
+    backSpeed: 35, // Vitesse de suppression (en millisecondes)
+    startDelay: 500, // Délai avant de commencer à écrire
+    backDelay: 1500, // Délai après avoir effacé un texte
+    loop: true, // Répéter en boucle
+    showCursor: false,
+    };
+  
+    var typed = new Typed("#typed-output-fe", options);
